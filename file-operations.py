4a4781d23c33e5fdd224fda1455b0148dd6a48cf
Aránzazu <zudealbertisdelc@gmail.com>
2024-02-20 15:57:39 +0100

    Initial commit

---
+++ file-operations.py
@@ -0,0 +1,11 @@
+file_name = "x-file.txt"
+fd = open(file_name, "a") # a for append, w for write, r for read
+
+while True:
+    line = input("Enter a line (or just press Enter to quit): ")
+    if line:
+        fd.write(line + "\n")
+    else:
+        break
+
+fd.close()
---
+++ functions.py
@@ -0,0 +1,42 @@
+def greet(name):
+    """
+    Simple greet function, prints hello
+    :param name: string
+    :return: None
+    """
+    print(f"Hello, {name}")
+
+# greet("Bogdan")
+# greet("John")
+# greet("Maria")
+
+def special_op(x, y, z=10):
+    """
+    Some special operation
+    :param x: int or float
+    :param y: int or float
+    :param z: int or float
+    :return: the result of the operation
+    """
+    return x * y + z
+
+
+result = special_op(2, 3, 4)
+print(result)
+print(special_op(2, 3))
+print(special_op(z=2, x=3, y=4))
+
+greet(special_op(2, 3, 4))
+
+#recursive function:
+def factorial(n):
+    """
+    Factorial of a number
+    :param n: int number
+    :return: the value of n!
+    """
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(5))
